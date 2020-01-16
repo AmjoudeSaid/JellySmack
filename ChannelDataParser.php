e4bf3256a1d849efb85be2a8bbd2a1688cc40d1a
Storm <funnygames350@gmail.com>
2020-01-16 14:28:11 +0100

    oui

--- ChannelDataParser.php
+++ ChannelDataParser.php
@@ -6,6 +6,9 @@
 $urlPart1 = "https://www.tiktok.com/node/share/user/";
 $chaine = "@gamology";
 $url = $urlPart1 . $chaine;
+$json = file_get_contents($url);
+$jsonArray = json_decode($json);
 
-echo json_decode($url);
+echo $jsonArray;
+
 ?>
