c875967b82bf9f42837b36d2e28f0f1a60b64ba0
CAAnthony <anthony.careddu23@gmail.com>
2020-01-16 14:55:29 +0100

    Create ChannelDataParserCareddu.php

---
+++ ChannelDataParserCareddu.php
@@ -0,0 +1,35 @@
+<?php
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://www.tiktok.com/node/share/user/@ohmygoal",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_SSL_VERIFYHOST=>0,
+  CURLOPT_SSL_VERIFYPEER=>0,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "GET",
+  CURLOPT_POSTFIELDS => "",
+  CURLOPT_HTTPHEADER => array(
+    "Postman-Token: e8be3338-ba96-457d-b107-b5f9b52b0eaf",
+    "cache-control: no-cache"
+  ),
+));
+curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+    'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:71.0) Gecko/20100101 Firefox/71.0',
+    'Host: www.tiktok.com'
+));
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  	echo $response;
+}
+?>
