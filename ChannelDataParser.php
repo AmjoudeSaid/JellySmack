d4dffeda3119990ad805afb57a4b8bcd118db116
Storm <funnygames350@gmail.com>
2020-01-16 15:32:14 +0100

    Affichage fans pour chaque chaine

--- ChannelDataParser.php
+++ ChannelDataParser.php
@@ -1,46 +1,48 @@
 <?php
 
-//$lstChaines = ["@gamology", "@ohmygoal", "@beautylicious", "@beautyhacks", "@beautywow", "@thisjusthappened",
-  //  "@abcdiyus", "@naturee", "@namemeifyoucan", "@boysdoitto"];
+$lstChaines = ["@gamology", "@ohmygoal", "@beautylicious", "@beautyhacks", "@beautywow", "@thisjusthappened",
+    "@abcdiyus", "@naturee", "@namemeifyoucan", "@boysdoitto"];
 
 
 $curl = curl_init();
-
 $urlPart1 = "https://www.tiktok.com/node/share/user/";
-$chaine = "@gamology";
-$url = $urlPart1 . $chaine;
 
-curl_setopt_array($curl, array(
-    CURLOPT_URL => $url,
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_SSL_VERIFYHOST => 0,
-    CURLOPT_SSL_VERIFYPEER => 0,
-    CURLOPT_ENCODING => "",
-    CURLOPT_MAXREDIRS => 10,
-    CURLOPT_TIMEOUT => 30,
-    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-    CURLOPT_CUSTOMREQUEST => "GET",
-    CURLOPT_POSTFIELDS => "",
-    CURLOPT_HTTPHEADER => array(
-        "Postman-Token: e8be3338-ba96-457d-b107-b5f9b52b0eaf",
-        "cache-control: no-cache"
-    ),
-));
-curl_setopt($curl, CURLOPT_HTTPHEADER, array(
-    'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:71.0) Gecko/20100101 Firefox/71.0',
-    'Host: www.tiktok.com'
-));
-$response = curl_exec($curl);
-$err = curl_error($curl);
+foreach ($lstChaines as $uneChaine) {
+    $chaine = $uneChaine;
+    $url = $urlPart1 . $chaine;
 
-curl_close($curl);
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => $url,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_SSL_VERIFYHOST=>0,
+        CURLOPT_SSL_VERIFYPEER=>0,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "GET",
+        CURLOPT_POSTFIELDS => "",
+        CURLOPT_HTTPHEADER => array(
+            "Postman-Token: e8be3338-ba96-457d-b107-b5f9b52b0eaf",
+            "cache-control: no-cache"
+        ),
+    ));
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+        'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:71.0) Gecko/20100101 Firefox/71.0',
+        'Host: www.tiktok.com'
+    ));
+    $response = curl_exec($curl);
+    $err = curl_error($curl);
 
-if ($err) {
-    echo "cURL Error #:" . $err;
-} else {
-    echo $response;
+    curl_close($curl);
+
+    if ($err) {
+        echo "cURL Error #:" . $err;
+    } else {
+        //echo $response;
+
+        $jsonArray = json_decode($response);
+        echo $jsonArray->body->userData->fans;
+    }
 }
-$jsonArray = json_decode($response);
-foreach ($jsonArray as $key => $value) {
-    echo $key . $value;
-}
+
